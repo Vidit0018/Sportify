d8ac05c88267aff79cc35ec53a702cae612519cc
Vidit <Vidit.msit@gmail.com>
2024-02-09 18:25:58 +0530

    db changed to mongo cloud

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,12 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Info").then(()=>{
+mongoose.connect("mongodb+srv://vidit0018:Gk95I8WbPmpiGsQO@sportify.nkdbgdb.mongodb.net/").then(()=>{
     console.log("connection successfull");
 }).catch((e)=>{
     console.log("error cought",e);
 })
 
 module.exports=mongoose;
+
+
+
+
