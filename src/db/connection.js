2d9a03e6ef52b6ea5d0c2e57f018dfab25fb4c14
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-10-15 21:42:13 +0530

    changes tracked recent

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://vidit0018:Gk95I8WbPmpiGsQO@sportify.nkdbgdb.mongodb.net/").then(()=>{
+mongoose.connect("mongodb+srv://chaudharyrajan387:S9CGsVUyaVv8tNYi@cluster0.uuuvt.mongodb.net/").then(()=>{
     console.log("connection successfull");
 }).catch((e)=>{
     console.log("error cought",e);
